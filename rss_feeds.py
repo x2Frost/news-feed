d2a7adeb13f8c216b26214ce215b0608d8a742a6
x2Frost <nicolas.creel@gmail.com>
2025-10-30 12:21:29 +0100

    Create rss_feeds.py

---
+++ rss_feeds.py
@@ -0,0 +1,38 @@
+# rss_feeds.py
+# Liste des flux RSS par thématique
+
+RSS_FEEDS = {
+    "datacenter": [
+        "https://www.lemondeinformatique.fr/flux-rss/datacenter.xml",
+        "https://www.01net.com/rss/actualites/datacenter/"
+    ],
+    "hardware": [
+        "https://www.lemondeinformatique.fr/flux-rss/hardware.xml",
+        "https://www.01net.com/rss/actualites/materiel/"
+    ],
+    "internet": [
+        "https://www.lemondeinformatique.fr/flux-rss/internet.xml"
+    ],
+    "logiciel": [
+        "https://www.lemondeinformatique.fr/flux-rss/logiciel.xml"
+    ],
+    "réseau": [
+        "https://www.lemondeinformatique.fr/flux-rss/reseau.xml"
+    ],
+    "sécurité": [
+        "https://www.lemondeinformatique.fr/flux-rss/securite.xml"
+    ],
+    "stockage": [
+        "https://www.lemondeinformatique.fr/flux-rss/stockage.xml"
+    ],
+    "télécoms": [
+        "https://www.lemondeinformatique.fr/flux-rss/telecoms.xml",
+        "https://www.01net.com/rss/actualites/telecom/"
+    ],
+    "actualité": [
+        "https://www.lemondeinformatique.fr/flux-rss/actualites.xml"
+    ],
+    "technos": [
+        "https://www.lemondeinformatique.fr/flux-rss/technologie.xml"
+    ]
+}
